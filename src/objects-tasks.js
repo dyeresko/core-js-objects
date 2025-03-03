9451efd853cc9869a819e3f58f2d59b5b641d638
Denis Yeresko <denis.yeresko@gmail.com>
2025-03-03 20:31:57 +0200

    feat: finish task fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -249,8 +249,9 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = Object.setPrototypeOf(JSON.parse(json), proto);
+  return obj;
 }
 
 /**
